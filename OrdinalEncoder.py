eaf918326b3f9d51bb89d4efc164742afe8e1214
Dutta-06 <odwitiyo.dutta@gmail.com>
2024-10-23 20:27:24 +0530

    Update OrdinalEncoder.py

--- OrdinalEncoder.py
+++ OrdinalEncoder.py
@@ -8,15 +8,15 @@ def OrdinalEncoder(column):
             print("Column cannot be encoded!")
     except:
         print("Column not found!")
-    categories = list(data[column].unique())
-    RangeList = list(range(len(categories)))
-    dict = {}
-    for i in categories:
-        dict[i] = random.choice(RangeList)
-        RangeList.remove(dict[i])
-    EncoderList = []
-    for j in data[column]:
-        EncoderList.append(dict[j])
-    data[column] = EncoderList
-    print(data)
-	
+    if data[column].dtype == 'object': 
+        categories = list(data[column].unique())
+        RangeList = list(range(len(categories)))
+        dict = {}
+        for i in categories:
+            dict[i] = random.choice(RangeList)
+            RangeList.remove(dict[i])
+        EncoderList = []
+        for j in data[column]:
+            EncoderList.append(dict[j])
+        data[column] = EncoderList
+        print(data)
